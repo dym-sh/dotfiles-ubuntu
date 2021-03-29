2be3de71c4603c25b17aa0eb152cd911f167625f
Dym Sohin <re@dym.sh>
2021-03-29 23:23:19 +0200

    added /fonts for emoji config

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/zsh
 
+
 # uninstall outdated noto-emoji
 sudo apt-get remove -y \
   fonts-noto-color-emoji
 
+
 # get latest noto-emoji
 mkdir -p ~/.fonts/NotoEmoji
 
-curl -L https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip \
-  -o ~/.fonts/NotoEmoji/emoji.zip
-unzip -un ~/.fonts/NotoEmoji/emoji.zip \
-  -d ~/.fonts/NotoEmoji/
-rm ~/.fonts/NotoEmoji/emoji.zip
-
-curl -L https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip \
-  -o ~/.fonts/NotoEmoji/color.zip
-unzip -un ~/.fonts/NotoEmoji/color.zip \
-  -d ~/.fonts/NotoEmoji/
-rm ~/.fonts/NotoEmoji/color.zip
+curl -L https://raw.githubusercontent.com/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf \
+  -o ~/.fonts/NotoEmoji/NotoColorEmoji.ttf
 
 
 # link font-priority config
 ln -s `pwd`/.fonts.conf ~/ # --force
+
+
+# rebuild fonts-cache
+fc-cache -fv
