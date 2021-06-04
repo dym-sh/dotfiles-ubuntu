5f298d631c508ac9b31d0b3f1e6bc2273655d580
Dym Sohin <re@dym.sh>
2021-06-04 16:57:51 +0200

    __ 20210604@165751

--- _installs/docker.sh
+++ _installs/docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-# install docker
+# install surrounding necessities
 sudo apt install -y \
   apt-transport-https \
   ca-certificates \
@@ -8,15 +8,16 @@ sudo apt install -y \
   gnupg \
   lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+# docker.com key file
+curl -fsSL 'https://download.docker.com/linux/ubuntu/gpg' \
   | sudo gpg --dearmor -o \
-  /usr/share/keyrings/docker-archive-keyring.gpg
+    '/usr/share/keyrings/docker-archive-keyring.gpg'
 
 
 # the `lsb_release` prints linux-mint's codename
-# of which docker has no idea
+# of which docker has no idea, so
 U='UBUNTU_CODENAME='
-DISTRO=`cat /etc/os-release | grep $U`
+DISTRO=`cat '/etc/os-release' | grep "$U"`
 if [ ! -z "$DISTRO" ]; then
   DISTRO="${DISTRO/$U/}"
 else
@@ -25,6 +26,7 @@ fi
 echo "DISTRO: '$DISTRO'"
 
 
+# add docker apt
 echo "
 deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu \
@@ -32,6 +34,10 @@ $DISTRO stable \
 " | sudo tee /etc/apt/sources.list.d/docker.list
 
 
+# update sources
 sudo apt update -y
+
+
+# install docker
 sudo apt install -y \
   docker-ce docker-ce-cli containerd.io
